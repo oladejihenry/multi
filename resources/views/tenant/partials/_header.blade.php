af259f73a66df00e9ada61bd2e985f5701bd3b98
Henry Oladeji <henryoladeji2007@gmail.com>
2023-07-18 21:44:47 +0100

    Modified Registration Page

--- resources/views/tenant/partials/_header.blade.php
+++ resources/views/tenant/partials/_header.blade.php
@@ -19,5 +19,88 @@
   <!-- END : Theme Config js-->
 </head>
 <body class=" font-inter skin-default">
+  <div class="loginwrapper">
+    <div class="lg-inner-column">
+      <div class="left-column relative z-[1]">
+        <div class="max-w-[520px] pt-20 ltr:pl-20 rtl:pr-20">
+          <a href="index.html">
+            <img src="{{ asset('tenant/dashboard-asset/assets/images/logo/logo.svg')}}" alt="" class="mb-10 dark_logo">
+            <img src="{{ asset('tenant/dashboard-asset/assets/images/logo/logo-white.svg')}}" alt="" class="mb-10 white_logo">
+          </a>
+          <h4>
+            Unlock your Project
+            <span class="text-slate-800 dark:text-slate-400 font-bold">
+                            performance
+                        </span>
+          </h4>
+        </div>
+        <div class="absolute left-0 2xl:bottom-[-160px] bottom-[-130px] h-full w-full z-[-1]">
+          <img src="{{ asset('tenant/dashboard-asset/assets/images/auth/ils1.svg')}}" alt="" class=" h-full w-full object-contain">
+        </div>
+      </div>
+      <div class="right-column  relative">
+        <div class="inner-content h-full flex flex-col bg-white dark:bg-slate-800">
+          <div class="auth-box h-full flex flex-col justify-center">
+            <div class="mobile-logo text-center mb-6 lg:hidden block">
+              <a href="index.html">
+                <img src="{{ asset('tenant/dashboard-asset/assets/images/logo/logo.svg')}}" alt="" class="mb-10 dark_logo">
+                <img src="{{ asset('tenant/dashboard-asset/assets/images/logo/logo-white.svg')}}" alt="" class="mb-10 white_logo">
+              </a>
+            </div>
+            <div class="text-center 2xl:mb-10 mb-4">
+              <h4 class="font-medium">Sign in</h4>
+              <div class="text-slate-500 text-base">
+                Sign in to your account to start using Dashcode
+              </div>
+            </div>
+    <!-- Conent start --->
   @yield('body')
+   <div class="relative border-b-[#9AA2AF] border-opacity-[16%] border-b pt-6">
+              <div class="absolute inline-block bg-white dark:bg-slate-800 dark:text-slate-400 left-1/2 top-1/2 transform -translate-x-1/2
+                                    px-4 min-w-max text-sm text-slate-500 font-normal">
+                Or continue with
+              </div>
+            </div>
+            <div class="max-w-[242px] mx-auto mt-8 w-full">
+
+              <!-- BEGIN: Social Log in Area -->
+              <ul class="flex">
+                <li class="flex-1">
+                  <a href="#" class="inline-flex h-10 w-10 bg-[#1C9CEB] text-white text-2xl flex-col items-center justify-center rounded-full">
+                    <img src="{{ asset('tenant/dashboard-asset/assets/images/icon/tw.svg')}}" alt="">
+                  </a>
+                </li>
+                <li class="flex-1">
+                  <a href="#" class="inline-flex h-10 w-10 bg-[#395599] text-white text-2xl flex-col items-center justify-center rounded-full">
+                    <img src="{{ asset('tenant/dashboard-asset/assets/images/icon/fb.svg')}}" alt="">
+                  </a>
+                </li>
+                <li class="flex-1">
+                  <a href="#" class="inline-flex h-10 w-10 bg-[#0A63BC] text-white text-2xl flex-col items-center justify-center rounded-full">
+                    <img src="{{ asset('tenant/dashboard-asset/assets/images/icon/in.svg')}}" alt="">
+                  </a>
+                </li>
+                <li class="flex-1">
+                  <a href="#" class="inline-flex h-10 w-10 bg-[#EA4335] text-white text-2xl flex-col items-center justify-center rounded-full">
+                    <img src="{{ asset('tenant/dashboard-asset/assets/images/icon/gp.svg')}}" alt="">
+                  </a>
+                </li>
+              </ul>
+              <!-- END: Social Log In Area -->
+            </div>
+            <div class="md:max-w-[345px] mx-auto font-normal text-slate-500 dark:text-slate-400 mt-8 uppercase text-sm">
+              <span>ALREADY REGISTERED?
+                            </span>
+              <a href="signin-one.html" class="text-slate-900 dark:text-white font-medium hover:underline">
+                Sign In
+              </a>
+            </div>
+          </div>
+          <div class="auth-footer text-center">
+            Copyright 2021, Dashcode All Rights Reserved.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
